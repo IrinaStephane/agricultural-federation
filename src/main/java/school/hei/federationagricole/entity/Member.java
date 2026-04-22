2b037c6e96a52bc6f3f3174d47b6238f978363f6
Irina <irinarakoto2005@gmail.com>
2026-04-22 09:09:11 +0300

    feat: add validatorÃ

--- src/main/java/school/hei/federationagricole/entity/Member.java
+++ src/main/java/school/hei/federationagricole/entity/Member.java
@@ -1,21 +1,59 @@
 package school.hei.federationagricole.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import school.hei.federationagricole.entity.enums.Gender;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
-@EqualsAndHashCode(callSuper = true)
-public class Member extends MemberInformation {
-    private MemberIdentifier id;
-    private List<Member> referees;
-    private boolean registrationFeePaid;
-    private boolean membershipDuesPaid;
+public class Member {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Instant enrolmentDate;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String profession;
+    private Gender gender;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Member> referees = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<MemberCollectivity> memberCollectivities = new ArrayList<>();
+
+    public boolean isAValidSponsor() {
+        return enrolmentDate != null &&
+                Duration.between(enrolmentDate, Instant.now()).toDays() >= 90;
+    }
+
+    public List<Integer> getIdsOfActualBelongingCollectivities() {
+        if (memberCollectivities == null) return List.of();
+
+        return memberCollectivities.stream()
+                .filter(mc -> mc.getEndDate() == null)
+                .map(mc -> mc.getCollectivity().getId())
+                .toList();
+    }
+    public List<Integer> getReferees(){
+        return this.referees.stream().map(Member::getId).toList();
+    }
+
+
 }
---
+++ src/main/java/school/hei/federationagricole/exception/PaymentException.java
@@ -0,0 +1,7 @@
+package school.hei.federationagricole.exception;
+
+public class PaymentException extends RuntimeException{
+    public PaymentException(String message){
+        super(message);
+    }
+}
