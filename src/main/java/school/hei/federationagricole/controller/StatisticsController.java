fefd4f3c7315ba6502339afd4bcfa7b37b1d66db
Irina <irinarakoto2005@gmail.com>
2026-05-06 16:24:08 +0300

    fix: change collectivites to collectivities

--- src/main/java/school/hei/federationagricole/controller/StatisticsController.java
+++ src/main/java/school/hei/federationagricole/controller/StatisticsController.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/collectivites")
+@RequestMapping("/collectivities")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
